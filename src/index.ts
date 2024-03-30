042365be551b4c27bebacabf2094ff62574aacbb
Stanley Ng <stanley121499@gmail.com>
2024-03-30 14:18:12 +0800

    pleaseeee

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
+const port: number = parseInt(process.env.PORT as string, 10) || 3000; // Default to 3000 if environment variable not set
 
 // Middleware
 app.use(cors()); // Enable CORS for all requests
@@ -18,28 +19,25 @@ app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Strive Clone App Backend API' });
 });
 
-// accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
-// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  // check the mode and token sent are correct
-  if (mode === "subscribe" && token === "AIErZ0xweiBhCHPvPM0oMAQ9zD89KjYg") {
-    // respond with 200 OK and challenge token from the request
+  if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) { // Use VERIFY_TOKEN from your .env file
     res.status(200).send(challenge);
     console.log("Webhook verified successfully!");
   } else {
-    // respond with '403 Forbidden' if verify tokens do not match
     res.sendStatus(403);
   }
 });
-
 
 app.post('/webhook', handleWebhook);
 
 // The error handler must be the last piece of middleware added to the app
 app.use(errorHandler);
 
-export default app;
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
