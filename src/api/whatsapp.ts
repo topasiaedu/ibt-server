1708d3c6d4f66bbc44b7a455ec9311d36dab43ea
Stanley Ng <stanley121499@gmail.com>
2024-06-06 17:02:00 +0800

    sync

--- src/api/whatsapp.ts
+++ src/api/whatsapp.ts
@@ -1,16 +1,17 @@
 // /api/whatsapp.ts
-import axios, { AxiosResponse } from 'axios';
-import { TemplateMessagePayload } from '../models/whatsapp/templateTypes';
-import { logError } from '../utils/errorLogger';
-import supabase from '../db/supabaseClient';
+import axios, { AxiosResponse } from 'axios'
+import { TemplateMessagePayload } from '../models/whatsapp/templateTypes'
+import { logError } from '../utils/errorLogger'
+import supabase from '../db/supabaseClient'
 
-const whatsappApiURL: string = 'https://graph.facebook.com/v19.0/';
-const token: string = 'EAAFZCUSsuZBkQBO7vI52BiAVIVDPsZAATo0KbTLYdZBQ7hCq59lPYf5FYz792HlEN13MCPGDaVP93VYZASXz9ZBNXaiATyIToimwDx0tcCB2sz0TwklEoof3K0mZASJtcYugK1hfdnJGJ1pnRXtnTGmlXiIgkyQe0ZC2DOh4qZAeRhJ9nd9hgKKedub4eaCgvZBWrOHBa3NadCqdlZCx0zO'; // Use environment variables for sensitive data
+const whatsappApiURL: string = 'https://graph.facebook.com/v19.0/'
+const token: string =
+  'EAAFZCUSsuZBkQBO7vI52BiAVIVDPsZAATo0KbTLYdZBQ7hCq59lPYf5FYz792HlEN13MCPGDaVP93VYZASXz9ZBNXaiATyIToimwDx0tcCB2sz0TwklEoof3K0mZASJtcYugK1hfdnJGJ1pnRXtnTGmlXiIgkyQe0ZC2DOh4qZAeRhJ9nd9hgKKedub4eaCgvZBWrOHBa3NadCqdlZCx0zO' // Use environment variables for sensitive data
 
 const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`
-};
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${token}`,
+}
 
 // {
 // 	"messaging_product": "whatsapp",
@@ -23,142 +24,210 @@ const headers = {
 // }
 
 export type MessagePayload = {
-    messaging_product: string;
-    recipient_type: string;
-    to: string;
-    type: string;
-    text: {
-        body: string;
-    }
+  messaging_product: string
+  recipient_type: string
+  to: string
+  type: string
+  text: {
+    body: string
+  }
 }
-
 
 /**
  * Send a message using a predefined template.
  * @param payload TemplateMessage - The message payload conforming to the TemplateMessage interface.
  */
-const sendMessageWithTemplate = async (payload: TemplateMessagePayload, phone_number_id: string): Promise<AxiosResponse<any>> => {
-    try {
-        // Check payload for spintax and replace with random value
-        const spintaxRegex = /{[^{}]*}/g;
+const sendMessageWithTemplate = async (
+  payload: TemplateMessagePayload,
+  phone_number_id: string
+): Promise<AxiosResponse<any>> => {
+  try {
+    // Check payload for spintax and replace with random value
+    const spintaxRegex = /{[^{}]*}/g
 
-        payload.template.components.forEach((component) => {
-            component.parameters.forEach((parameter) => {
-                if (parameter.type === 'text' && parameter.text) {
-                    const matches = parameter.text.match(spintaxRegex);
-                    if (matches) {
-                    matches.forEach((spintax) => {
-                            const spintaxOptions = spintax.slice(1, -1).split('|');
-                            const randomOption = spintaxOptions[Math.floor(Math.random() * spintaxOptions.length)];
-                            parameter.text = parameter.text?.replace(spintax, randomOption);
-                        });
-                    }
-                }
-            });
-        });
+    payload.template.components.forEach((component) => {
+      component.parameters.forEach((parameter) => {
+        if (parameter.type === 'text' && parameter.text) {
+          const matches = parameter.text.match(spintaxRegex)
+          if (matches) {
+            matches.forEach((spintax) => {
+              const spintaxOptions = spintax.slice(1, -1).split('|')
+              const randomOption =
+                spintaxOptions[
+                  Math.floor(Math.random() * spintaxOptions.length)
+                ]
+              parameter.text = parameter.text?.replace(spintax, randomOption)
+            })
+          }
+        }
+      })
+    })
 
-        const response = await axios.post(`${whatsappApiURL}/${phone_number_id}/messages`, payload, { headers });
-        return response;
-    } catch (error) {
-        logError(error as Error, 'Error sending message with template. Payload: ' + JSON.stringify(payload, null, 2) + '\n');
-        throw new Error('Failed to send message with template');
-    }
-};
-
-const sendMessage = async (payload: MessagePayload, phone_number_id: string): Promise<AxiosResponse<any>> => {
-    try {
-        const response = await axios.post(`${whatsappApiURL}/${phone_number_id}/messages`, payload, { headers });
-        return response;
-    } catch (error) {
-        logError(error as Error, 'Error sending message. Payload: ' + JSON.stringify(payload, null, 2) + '\n');
-        throw new Error('Failed to send message');
-    }
-};
-
-const fetchTemplatesService = async (WABA_ID: string): Promise<AxiosResponse<any>> => {
-    try {
-        const response = await axios.get(`${whatsappApiURL}/${WABA_ID}/message_templates`, { headers });
-        return response;
-    } catch (error) {
-        logError(error as Error, 'Error fetching templates with WABA ID: ' + WABA_ID + '\n');
-        throw new Error('Failed to fetch templates');
-    }
+    const response = await axios.post(
+      `${whatsappApiURL}/${phone_number_id}/messages`,
+      payload,
+      { headers }
+    )
+    return response
+  } catch (error) {
+    logError(
+      error as Error,
+      'Error sending message with template. Payload: ' +
+        JSON.stringify(payload, null, 2) +
+        '\n'
+    )
+    throw new Error('Failed to send message with template')
+  }
 }
 
-const fetchWABAPhoneNumbersService = async (WABA_ID: string): Promise<AxiosResponse<any>> => {
-    try {
-        const response = await axios.get(`${whatsappApiURL}/${WABA_ID}/phone_numbers`, { headers });
-        return response;
-    } catch (error) {
-        logError(error as Error, 'Error fetching phone numbers with WABA ID: ' + WABA_ID + '\n');
-        throw new Error('Failed to fetch phone numbers');
-    }
+const sendMessage = async (
+  payload: MessagePayload,
+  phone_number_id: string
+): Promise<AxiosResponse<any>> => {
+  try {
+    const response = await axios.post(
+      `${whatsappApiURL}/${phone_number_id}/messages`,
+      payload,
+      { headers }
+    )
+    return response
+  } catch (error) {
+    logError(
+      error as Error,
+      'Error sending message. Payload: ' +
+        JSON.stringify(payload, null, 2) +
+        '\n'
+    )
+    throw new Error('Failed to send message')
+  }
+}
+
+const fetchTemplatesService = async (
+  WABA_ID: string
+): Promise<AxiosResponse<any>> => {
+  try {
+    const response = await axios.get(
+      `${whatsappApiURL}/${WABA_ID}/message_templates`,
+      { headers }
+    )
+    return response
+  } catch (error) {
+    logError(
+      error as Error,
+      'Error fetching templates with WABA ID: ' + WABA_ID + '\n'
+    )
+    throw new Error('Failed to fetch templates')
+  }
+}
+
+const fetchWABAPhoneNumbersService = async (
+  WABA_ID: string
+): Promise<AxiosResponse<any>> => {
+  try {
+    const response = await axios.get(
+      `${whatsappApiURL}/${WABA_ID}/phone_numbers`,
+      { headers }
+    )
+    return response
+  } catch (error) {
+    logError(
+      error as Error,
+      'Error fetching phone numbers with WABA ID: ' + WABA_ID + '\n'
+    )
+    throw new Error('Failed to fetch phone numbers')
+  }
 }
 
 const fetchWABAsService = async (): Promise<AxiosResponse<any>> => {
-    try {
-        const response = await axios.get(`${whatsappApiURL}/1785814375085019/owned_whatsapp_business_accounts`, { headers });
-        return response;
-    } catch (error) {
-        logError(error as Error, 'Error fetching WABAs\n');
-        console.error('Error fetching WABAs:', JSON.stringify(error, null, 2));
-        throw new Error('Failed to fetch WABAs');
-    }
+  try {
+    const response = await axios.get(
+      `${whatsappApiURL}/1785814375085019/owned_whatsapp_business_accounts`,
+      { headers }
+    )
+    return response
+  } catch (error) {
+    logError(error as Error, 'Error fetching WABAs\n')
+    console.error('Error fetching WABAs:', JSON.stringify(error, null, 2))
+    throw new Error('Failed to fetch WABAs')
+  }
 }
 
 const fetchImageURL = async (imageId: string): Promise<AxiosResponse<any>> => {
-    try {
-        const response = await axios.get(`${whatsappApiURL}/${imageId}`, { headers });
-        return response;
-    } catch (error) {
-        logError(error as Error, 'Error fetching image with ID: ' + imageId + '\n');
-        throw new Error('Failed to fetch image');
-    }
+  try {
+    const response = await axios.get(`${whatsappApiURL}/${imageId}`, {
+      headers,
+    })
+    return response
+  } catch (error) {
+    logError(error as Error, 'Error fetching image with ID: ' + imageId + '\n')
+    throw new Error('Failed to fetch image')
+  }
 }
 
 const fetchMedia = async (imageId: string, randomFileName: string) => {
-    try {
-        // Assume axios and headers are set up previously
-        const response = await axios.get(`${whatsappApiURL}/${imageId}`, { headers });
+  try {
+    // Assume axios and headers are set up previously
+    const response = await axios.get(`${whatsappApiURL}/${imageId}`, {
+      headers,
+    })
 
-        // Fetch the actual image data as a buffer
-        const data = await axios.get(response.data.url, {
-            headers: { 'Authorization': `Bearer ${token}` },
-            responseType: 'arraybuffer'
-        });
+    // Fetch the actual image data as a buffer
+    const data = await axios.get(response.data.url, {
+      headers: { Authorization: `Bearer ${token}` },
+      responseType: 'arraybuffer',
+    })
 
-        if (response.status !== 200) {
-            throw new Error('Failed to fetch image');
-        }
-
-        const contentType = data.headers['content-type'] || 'image/jpeg'; // Default if no content type is provided
-
-        // Upload the buffer directly to Supabase storage
-        const { data: uploadData, error } = await supabase.storage
-            .from('media')
-            .upload(randomFileName, data.data, {
-                contentType: contentType,
-                upsert: true
-            });
-
-        if (error) throw error;
-
-        return `https://yvpvhbgcawvruybkmupv.supabase.co/storage/v1/object/public/media/` + uploadData.path;
-    } catch (error) {
-        console.error('Error fetching or uploading image with ID:', imageId, error);
-        throw new Error('Failed to fetch or upload image');
+    if (response.status !== 200) {
+      throw new Error('Failed to fetch image')
     }
+
+    const contentType = data.headers['content-type'] || 'image/jpeg' // Default if no content type is provided
+
+    // Upload the buffer directly to Supabase storage
+    const { data: uploadData, error } = await supabase.storage
+      .from('media')
+      .upload(randomFileName, data.data, {
+        contentType: contentType,
+        upsert: true,
+      })
+
+    if (error) throw error
+
+    return (
+      `https://yvpvhbgcawvruybkmupv.supabase.co/storage/v1/object/public/media/` +
+      uploadData.path
+    )
+  } catch (error) {
+    console.error('Error fetching or uploading image with ID:', imageId, error)
+    throw new Error('Failed to fetch or upload image')
+  }
 }
 
 const subscribeWebhook = async (WABA_ID: string) => {
-    // https://graph.facebook.com/v19.0/278010752057306/subscribed_apps
-    try {
-        const response = await axios.post(`${whatsappApiURL}/${WABA_ID}/subscribed_apps?access_token=${token}`, {}, { headers });
-        return response;
-    } catch (error) {
-        logError(error as Error, 'Error subscribing webhook with WABA ID: ' + WABA_ID + '\n');
-        throw new Error('Failed to subscribe webhook');
-    }
+  // https://graph.facebook.com/v19.0/278010752057306/subscribed_apps
+  try {
+    const response = await axios.post(
+      `${whatsappApiURL}/${WABA_ID}/subscribed_apps?access_token=${token}`,
+      {},
+      { headers }
+    )
+    return response
+  } catch (error) {
+    logError(
+      error as Error,
+      'Error subscribing webhook with WABA ID: ' + WABA_ID + '\n'
+    )
+    throw new Error('Failed to subscribe webhook')
+  }
 }
 
-export { sendMessageWithTemplate, fetchTemplatesService, fetchWABAPhoneNumbersService, fetchWABAsService, fetchImageURL, fetchMedia, subscribeWebhook, sendMessage };
+export {
+  sendMessageWithTemplate,
+  fetchTemplatesService,
+  fetchWABAPhoneNumbersService,
+  fetchWABAsService,
+  fetchImageURL,
+  fetchMedia,
+  subscribeWebhook,
+  sendMessage,
+}
--- src/webhook/whatsapp/helpers/insertImageMessage.ts
+++ src/webhook/whatsapp/helpers/insertImageMessage.ts
@@ -1,13 +1,16 @@
+import supabase from '../../../db/supabaseClient'
+import { logError } from '../../../utils/errorLogger'
+import { fetchMedia } from '../../../api/whatsapp'
 
-import supabase from '../../../db/supabaseClient';
-import { logError } from '../../../utils/errorLogger';
-import { fetchMedia } from '../../../api/whatsapp';
-
-const insertImageMessage = async (message: any, display_phone_number: string, project_id: string) => {
-  console.log('Inserting image message into database'	)
+const insertImageMessage = async (
+  message: any,
+  display_phone_number: string,
+  project_id: string
+) => {
+  console.log('Inserting image message into database')
   try {
-    const { from, id, timestamp, type, image } = message;
-    const { id: imageId, caption } = image;
+    const { from, id, timestamp, type, image } = message
+    const { id: imageId, caption } = image
 
     // Check if the database has the same wa_message_id
     let { data: existingMessage, error: findError } = await supabase
@@ -17,7 +20,7 @@ const insertImageMessage = async (message: any, display_phone_number: string, pr
       .single()
 
     if (existingMessage?.wa_message_id === id) {
-      return 'Message already exists in the database';
+      return 'Message already exists in the database'
     }
 
     // Find the contact_id of the sender
@@ -26,59 +29,77 @@ const insertImageMessage = async (message: any, display_phone_number: string, pr
       .select('contact_id')
       .eq('wa_id', from)
       .eq('project_id', project_id)
-      .single();
+      .single()
 
     if (senderError) {
-      console.error('Error finding sender in database:', senderError);
-      throw senderError;
+      console.error('Error finding sender in database:', senderError)
+      throw senderError
     }
 
     if (!sender) {
-      throw new Error('Sender not found in database');
+      throw new Error('Sender not found in database')
     }
 
-    const senderId = sender.contact_id;
+    const senderId = sender.contact_id
 
     const myPhoneNumberId = await supabase
       .from('phone_numbers')
       .select('phone_number_id')
       .eq('number', display_phone_number)
-      .single();
+      .single()
 
-    const myPhoneNumber = myPhoneNumberId?.data?.phone_number_id;
+    const myPhoneNumber = myPhoneNumberId?.data?.phone_number_id
 
-    // Generate random file name 
-    const fileName = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    // Generate random file name
+    const fileName =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15)
 
-    const media = await fetchMedia(imageId, fileName);
+    const media = await fetchMedia(imageId, fileName)
 
     if (!media) {
-      throw new Error('Error fetching media from WhatsApp API');
+      throw new Error('Error fetching media from WhatsApp API')
     }
 
     // Insert the message into the database
     let { error: messageError } = await supabase
       .from('messages')
-      .insert([{
-        contact_id: senderId,
-        message_type: type,
-        content: caption,
-        phone_number_id: myPhoneNumber,
-        wa_message_id: id,
-        direction: 'inbound',
-        media_url: media,
-        project_id,
-        status: 'received',
-      }])
-      .single();
+      .insert([
+        {
+          contact_id: senderId,
+          message_type: type,
+          content: caption,
+          phone_number_id: myPhoneNumber,
+          wa_message_id: id,
+          direction: 'inbound',
+          media_url: media,
+          project_id,
+          status: 'received',
+        },
+      ])
+      .single()
 
     if (messageError) {
-      logError(messageError as unknown as Error, 'Error inserting inbound image message into database. Data: ' + JSON.stringify(message, null, 2) + '\n Error: ' + JSON.stringify(messageError, null, 2));
+      logError(
+        messageError as unknown as Error,
+        'Error inserting inbound image message into database. Data: ' +
+          JSON.stringify(message, null, 2) +
+          '\n Error: ' +
+          JSON.stringify(messageError, null, 2)
+      )
     }
   } catch (error) {
-    logError(error as Error, 'Error inserting inbound text message into database. Data: ' + JSON.stringify(message, null, 2) + '\n Error: ' + JSON.stringify(error, null, 2) + '\n' + "Inside insertImageMessage function in insertImageMessage.ts");
-    return 'Error inserting inbound text message into database';
+    logError(
+      error as Error,
+      'Error inserting inbound text message into database. Data: ' +
+        JSON.stringify(message, null, 2) +
+        '\n Error: ' +
+        JSON.stringify(error, null, 2) +
+        '\n' +
+        'Inside insertImageMessage function in insertImageMessage.ts'
+    )
+    return 'Error inserting inbound text message into database'
   }
 }
 
-export default insertImageMessage;
+export default insertImageMessage
