def8978889f0acd7f41c56dfc0ceced64089b50a
topasiaedu <138757445+topasiaedu@users.noreply.github.com>
2024-07-09 17:39:14 +0800

    implemented new template update system

--- src/webhook/whatsapp/events/message_template_status_update.ts
+++ src/webhook/whatsapp/events/message_template_status_update.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import supabase from '../../../db/supabaseClient';
+import { fetchTemplatesFunction } from '../../../cronJobs/fetchTemplates';
+import { logError } from '../../../utils/errorLogger';
 
-// Example Response:
+// Example Value:
 // {
 //   "field": "message_template_status_update",
 //   "value": {
@@ -13,9 +15,25 @@ import supabase from '../../../db/supabaseClient';
 //   }
 // }
 
-const handleMessageTemplateStatusUpdate = async (req: Request, res: Response) => {
-  // Handle message template status updates
-  console.log('Handling message template status updates', req.body);
+const handleMessageTemplateStatusUpdate = async (value:any) => {
+  const { event, message_template_id, message_template_name, message_template_language, reason } = value;
+
+  // Try to update the message template status in the database, if it failed call the function fetchTemplatesFunction
+  const { data: updatedTemplate, error: updateError } = await supabase
+    .from('templates')
+    .update({
+      status: event,
+      reason: reason
+    })
+    .eq('wa_template_id', message_template_id)
+    .single()
+
+  if (updateError) {
+    console.error('Error updating template in database. Template ID: ' + message_template_id + '\n');
+    console.error(JSON.stringify(updateError, null, 2));
+    logError(updateError as unknown as Error, 'Error updating template in database. Template ID: ' + message_template_id + '\n');
+    fetchTemplatesFunction();
+  }
 }
 
 export default handleMessageTemplateStatusUpdate;
