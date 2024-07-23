88aff4f530c61cc701e0cdaf611a3a95c80f5d44
Stanley Ng <stanley121499@gmail.com>
2024-07-23 23:17:14 +0800

    Added Debugging lIne

--- src/webhook/whatsapp/events/messages.ts
+++ src/webhook/whatsapp/events/messages.ts
@@ -74,7 +74,7 @@ const handleOutgoingMessage = async (value: any) => {
           const date = new Date(
             parseInt(status.conversation.expiration_timestamp) * 1000
           );
-        
+        console.log("Existing Conversations: ", existingConversations)
           if (!existingConversations || existingConversations.length === 0) {
             console.log('No existing conversation found, inserting a new one.');
         
