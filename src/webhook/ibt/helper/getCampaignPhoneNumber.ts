35bc63f0de13cc835870635cc1f903c59ba2d35c
Stanley Ng <stanley121499@gmail.com>
2024-08-14 19:34:52 +0800

    Hotfix

--- src/webhook/ibt/helper/getCampaignPhoneNumber.ts
+++ src/webhook/ibt/helper/getCampaignPhoneNumber.ts
@@ -106,7 +106,7 @@ export const getWorkflowPhoneNumber = async (
         .select(
           '*,whatsapp_business_accounts(*,business_manager(*))'
         )
-        .eq('wa_id', contact.last_contacted_by)
+        .eq('phone_number_id', contact.last_contacted_by)
 
     if (lastContactedPhoneNumberError) {
       logError(
