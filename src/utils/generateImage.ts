8197174d82fe641628db40916f1edd2dc0ece668
topasiaedu <138757445+topasiaedu@users.noreply.github.com>
2024-08-25 22:27:44 +0800

    Sync to pc

--- src/utils/generateImage.ts
+++ src/utils/generateImage.ts
@@ -1,23 +1,7 @@
-import * as fabric from 'fabric';
-import { JSDOM } from 'jsdom';
-import * as canvas from 'canvas';
-import * as fs from 'fs/promises';
-import * as path from 'path';
-import { Contact } from '../db/contacts';
+import { fabric } from 'fabric'
+import { Contact } from '../db/contacts'
+import supabase from '../db/supabaseClient'
 
-// Setup jsdom and node-canvas
-const { document } = new JSDOM('<!DOCTYPE html><html><body></body></html>').window;
-(global as any).document = document;
-(global as any).window = document.defaultView as unknown as Window & typeof globalThis;
-(global as any).HTMLCanvasElement = canvas.Canvas as unknown as { new(): HTMLCanvasElement; prototype: HTMLCanvasElement; };
-(global as any).HTMLImageElement = canvas.Image as unknown as { new(): HTMLImageElement; prototype: HTMLImageElement; };
-(global as any).ImageData = canvas.ImageData;
-(global as any).DOMParser = new JSDOM().window.DOMParser;
-
-// const fontPath = path.join(__dirname, 'fonts', 'arial-unicode-ms.ttf');
-
-// Assuming you have a font file named 'YourChineseFont.ttf' in the 'fonts' directory
-// fabric.registerFont(fontPath, { family: 'Arial Unicode MS' });
 
 // Function to check if a string contains Chinese characters
 function containsChinese(text: string): boolean {
@@ -33,56 +17,41 @@ export async function generateImage(
     throw new Error('Canvas state or contact is missing')
   }
 
-  console.log('====================================')
-  console.log('Inside generateImage')
-
-  const uniqueID = Math.random().toString(36)
-
+  const uniqueID = Math.random().toString(36).substring(7)
   const width = canvasState.width || 500
   const height = canvasState.height || 500
-  // Declare canvas variable outside the try block
+
   let canvas: fabric.StaticCanvas
 
   try {
-    console.log('Before StaticCanvas initialization')
-    canvas = new fabric.StaticCanvas(undefined, {
-      width: width,
-      height: height,
-      backgroundColor: 'white',
-    })
-    console.log('After StaticCanvas initialization')
+    // Create a new Fabric canvas with dynamic dimensions
+    canvas = new fabric.StaticCanvas(null, { width, height, backgroundColor: 'white' });
   } catch (err) {
     console.error('Error during StaticCanvas initialization:', err)
-    throw err // Re-throw to handle it in calling code or further up the stack
+    throw err
   }
 
-  console.log('====================================')
-  console.log('Inside generateImage')
+  // Correct object types in canvasState before loading
   canvasState.objects = canvasState.objects.map((object: any) => {
-    try {
-      if (object.type === 'textbox') {
-        // Replace object.text with name if %name% is found
-        const text = object.text.replace(/%name%/g, contact.name)
-        return { ...object, text } // return a new object with the updated text
-      }
-      console.log('Object:', object)
-      return object
-    } catch (err) {
-      console.error(`Error updating text: ${(err as unknown as any).message}`)
-      throw new Error(`Error updating text: ${(err as unknown as any).message}`)
+    if (object.type === 'Rect') object.type = 'rect'
+    if (object.type === 'Textbox') object.type = 'textbox'
+    if (object.type === 'textbox') {
+      const text = object.text.replace(/%name%/g, contact.name)
+      return { ...object, text }
     }
+    return object
   })
 
-  // canvasState.objects.forEach((obj: any) => {
-  //   if (obj.type === 'textbox' && containsChinese(obj.text)) {
-  //     obj.fontFamily = 'Arial Unicode MS'
-  //   }
-  // })
+  console.log(
+    'Objects after mapping:',
+    JSON.stringify(canvasState.objects, null, 2)
+  )
 
-  console.log('Canvas state:', canvasState)
   return new Promise((resolve, reject) => {
     canvas.loadFromJSON(canvasState, async () => {
       try {
+        console.log('Loaded from JSON')
+
         canvas.forEachObject((obj: any) => {
           if (
             obj.type === 'text' &&
@@ -92,29 +61,56 @@ export async function generateImage(
           }
         })
 
-        const dataUrl = canvas.toDataURL({
+        canvas.renderAll()
+
+        const canvasDataURL = canvas.toDataURL({
           format: 'png',
           quality: 1,
           multiplier: 2,
         })
-        const base64Data = dataUrl.split(';base64,').pop()
-        const buffer = Buffer.from(base64Data!, 'base64')
+        const byteString = atob(canvasDataURL.split(',')[1])
+        const mimeString = canvasDataURL
+          .split(',')[0]
+          .split(':')[1]
+          .split(';')[0]
+        const arrayBuffer = new ArrayBuffer(byteString.length)
+        const uint8Array = new Uint8Array(arrayBuffer)
 
-        const directoryPath = path.join(
-          __dirname,
-          '..',
-          'public',
-          'images',
-          'personalized_image'
-        )
-        const filePath = path.join(directoryPath, `${uniqueID}.png`)
+        for (let i = 0; i < byteString.length; i++) {
+          uint8Array[i] = byteString.charCodeAt(i)
+        }
 
-        await fs.mkdir(directoryPath, { recursive: true })
-        await fs.writeFile(filePath, buffer)
+        const blob = new Blob([uint8Array], { type: mimeString })
 
-        console.log('File written to', filePath)
+        // Step 2: Upload the image to Supabase
+        const { error: uploadError } = await supabase.storage
+          .from('media')
+          .upload(`templates/${uniqueID}.png`, blob, {
+            contentType: 'image/png',
+            upsert: true,
+          })
 
-        const imageUrl = `https://ibt3.whatsgenie.com/images/personalized_image/${uniqueID}.png`
+        if (uploadError) {
+          throw new Error(
+            'Error uploading image to Supabase: ' + uploadError.message
+          )
+        }
+
+        // const directoryPath = path.join(
+        //   __dirname,
+        //   '..',
+        //   'public',
+        //   'images',
+        //   'personalized_image'
+        // );
+        // const filePath = path.join(directoryPath, `${uniqueID}.png`);
+
+        // await fs.mkdir(directoryPath, { recursive: true });
+        // await fs.writeFile(filePath, buffer);
+
+        // console.log('File written to', filePath);
+
+        const imageUrl = `https://yvpvhbgcawvruybkmupv.supabase.co/storage/v1/object/public/media/templates/${uniqueID}.png`
         resolve(imageUrl)
       } catch (err) {
         reject(`Error generating image: ${(err as unknown as any).message}`)
